b2dc4dc8d48971aeb40463e5ac442e5e2d846d10
Ben Chu <30444978+bennchu@users.noreply.github.com>
2017-07-30 21:48:54 -0700

    Add files via upload

---
+++ magic8.R
@@ -0,0 +1,13 @@
+
+magic8 = function(x){
+  {randoans = c("yes",
+               "no",
+               "yesno",
+               "noyes",
+               "meow meow")}
+  print(sample(randoans,1))
+  }
+
+magic8("what up")
+ 
+
